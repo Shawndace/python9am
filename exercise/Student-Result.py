57132fac5eb07838cf7bdf1224b5dd52fe194219
Shawndace <sandeshkc999@gmail.com>
2021-07-27 06:29:11 +0545

    'exercises'

--- exercise/Student-Result.py
+++ exercise/Student-Result.py
@@ -1,23 +1,41 @@
 nep = float(input("Enter marks of Nepali: "))
+if nep > 100:
+    print("Marks is more than 100")
+    exit()
+
 eng = float(input("Enter marks of English: "))
+if eng > 100:
+    print("Marks is more than 100")
+    exit()
+
 mat = float(input("Enter marks of math: "))
+if mat > 100:
+    print("Marks is more than 100")
+    exit()
 sci = float(input("enter marks of science: "))
+if sci > 100:
+    print("Marks is more than 100")
+    exit()
 pop = float(input("Enter marks of population: "))
+if pop > 100:
+    print("Marks is more than 100")
+    exit()
 
 total = nep + eng + mat + sci + pop
-perc = (total / 5)
+perc = total / 5
+division = ""
 
-if nep < 100 or eng < 100 or mat < 100 or sci < 100 or pop < 100:
-    if perc > 35 and perc < 45:
-        print("Third division")
-    elif perc > 45 and perc < 60:
-        print("second div")
-    elif perc > 60 and perc < 75:
-        print("first div")
-    elif perc > 75 and perc < 100:
-        print("Distiction")
-else:
-    print("Marks is invalid")
+if perc > 35 and perc < 45:
+    division += "Third"
+elif perc > 45 and perc < 60:
+    division += "second"
+elif perc > 60 and perc < 75:
+    division += "first"
+elif perc > 75 and perc < 100:
+    division += "distinction"
+
+print("=============================")
+
 
 # HOW TO VALID MARKS?
 
